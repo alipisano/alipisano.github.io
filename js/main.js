c4974abf86a1885ba5de42700f9ed01c399b8895
Ali Pisano <pisano.alexandra@gmail.com>
2016-05-11 17:59:53 -0700

    send page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // When user clicks "Get Started" Button
-$('button').on('click', function() {
+$('#start').on('click', function() {
 
 // Image on homepage appears
 
-$('nic').fadeIn();
+$('.nic').fadeIn();
 
 });
 
